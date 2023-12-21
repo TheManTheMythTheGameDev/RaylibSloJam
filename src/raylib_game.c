2c31ab2abe8dae6243e676a455f345fda01a2351
Amibitious-Jay <andrewjo257@gmail.com>
2023-12-21 21:58:31 +0100

    Smth to see if my git is connected

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -84,7 +84,7 @@ int main(void)
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
-    SetTargetFPS(60);     // Set our game frames-per-second
+    SetTargetFPS(6);     // Set our game frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
