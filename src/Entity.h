a1b525f0dd1badf6f431ce6c65543d202bb81499
Chase Mayer <thefatgalaboo@gmail.com>
2023-12-26 15:31:56 -0800

    this *might* work... ?

--- src/Entity.h
+++ src/Entity.h
@@ -6,9 +6,24 @@ class Entity
 {
 public:
     Entity();
-    ~Entity();
+    ~Entity()
+    {
+        for (component* comp : components)
+        {
+            delete comp;
+        }
+    }
 
+    template<typename T, typename... Args>
+    inline void AddComponent(Args... constructorArgs)
+    {
+        components.push_back((component*)(new T(constructorArgs)));
+    }
 
+    inline void AddComponent(component* comp)
+    {
+        components.push_back(comp);
+    }
 private:
     std::vector<component*> components;
 };
