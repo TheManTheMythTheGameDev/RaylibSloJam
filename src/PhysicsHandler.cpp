9b84d07bf9521025d21d6f6f02c725cdf14798ac
Amibitious-Jay <andrewjo257@gmail.com>
2023-12-27 22:01:34 +0100

    basic physics handler stuff

---
+++ src/PhysicsHandler.cpp
@@ -0,0 +1,26 @@
+#include "PhysicsHandler.h"
+
+void PhysicsHandler::CalculateSteps(float dTime) {
+    currentTime += dTime;
+    while (currentTime > stepTime) {
+        Step();
+        currentTime -= stepTime;
+    }
+}
+
+void PhysicsHandler::Step() {
+    for (PhysicsComponent* physicsEnt : physicsEntities) {
+        physicsEnt->velocity = Vector2{ 5, 5 };
+    }
+}
+
+void PhysicsHandler::AddEntity(PhysicsComponent* newEntity) {
+    physicsEntities.push_back(newEntity);
+}
+
+void PhysicsHandler::ClearEntities() {
+    int numEntities = physicsEntities.size();
+    for (int i = 0; i < numEntities; i++) {
+        physicsEntities.pop_back();
+    }
+}
